271b3c52d5ae06ba371208cd3b9aefc481612bf2
SimeJadrijev <sime.jadrijev@gmail.com>
2024-03-08 00:11:44 +0100

    Finished an initial version of Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import classes from "./../styles/header.module.css";
+
+const Header = () =>{
+
+    return (<>
+        <header>
+            <img id={classes["header-image"]} src="./src/assets/images/njuskalo header image.jpeg" alt="njuskalo header image" />
+            <input type="text" />
+            <button>Traži</button>
+        </header>
+    </>)
+}
+
+export default Header;
---
+++ src/pages/Products.jsx
@@ -0,0 +1,9 @@
+import Header from "../components/Header";
+
+const Products = () => {
+    return (
+        <Header />
+    )
+}
+
+export default Products;
