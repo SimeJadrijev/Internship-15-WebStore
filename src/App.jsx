d188d1557b7f69d1b7c0830d2e7a700a2c6a0d5d
SimeJadrijev <sime.jadrijev@gmail.com>
2024-03-08 16:39:04 +0100

    Implemented a container to display all products

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,21 @@ import Products from "./pages/Products";
 import ProductCard from "./components/ProductCard";
 import { useState } from "react";
 import { useEffect } from "react";
+import ProductCardsContainer from "./components/ProductCardsContainer";
 
 function App() {
-  const [product, setProduct] = useState(null);
+  const [products, setProducts] = useState(null);
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products/20")
+    fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
-      .then((data) => setProduct(data));
+      .then((data) => setProducts(data))
+      .catch((error) => console.error("Error fetching products:", error));
   }, []);
 
   return (
     <>
       <Products />
-      {product && <ProductCard product={product} />}
+      <ProductCardsContainer products={products} />
     </>
   );
 }
---
+++ src/components/ProductCardsContainer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import classes from "./../styles/product-card.module.css";
+import ProductCard from "./ProductCard";
+
+const ProductCardsContainer = ({ products }) => {
+  return (
+    <div className={classes["product-cards-container"]}>
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
+
+export default ProductCardsContainer;
