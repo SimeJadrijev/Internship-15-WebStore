993eacd7a4783ee1b97b9932ace78e5067616eae
SimeJadrijev <sime.jadrijev@gmail.com>
2024-03-10 19:26:11 +0100

    Implemented navigating to product's page by clicking on that specific product from the main page

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,15 +1,19 @@
 import classes from "./../styles/product-card.module.css";
+import { Link } from "react-router-dom";
 const ProductCard = ({ product }) => {
+  const path = `/product/${product.id}`;
   return (
     <>
-      <div className={classes["product-card"]}>
-        <img src={product.image} alt="${product.title} image" />
+      <Link to={path} className={classes["product-card-link"]}>
+        <div className={classes["product-card"]}>
+          <img src={product.image} alt="${product.title} image" />
 
-        <div className={classes["product-card-bottom"]}>
-          <h3 className={classes["product-card-title"]}>{product.title}</h3>
-          <h3 className={classes["product-card-price"]}>{product.price} €</h3>
+          <div className={classes["product-card-bottom"]}>
+            <h3 className={classes["product-card-title"]}>{product.title}</h3>
+            <h3 className={classes["product-card-price"]}>{product.price} €</h3>
+          </div>
         </div>
-      </div>
+      </Link>
     </>
   );
 };
